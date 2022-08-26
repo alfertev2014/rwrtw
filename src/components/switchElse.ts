4932a818e610ede4d76b1312317f0e624f9f28ef
Vasiliy Alfertev <vasiliy.alfertev@corp.codeinside.ru>
2022-08-26 12:48:33 +0300

    Implement switch component

---
+++ src/components/switchElse.ts
@@ -0,0 +1,58 @@
+import { ComponentFactory, Placeholder, plh, Renderer } from '../component'
+
+export interface Switch<T> {
+    value: T
+}
+
+export type CaseBranch<T> = [T, ComponentFactory | null]
+
+
+class SwitchImpl<T> implements Switch<T> {
+    _value: T
+    branches: CaseBranch<T>[]
+    defaultBranch: ComponentFactory | null
+    readonly placeholder: Placeholder
+    
+    constructor(
+        renderer: Renderer,
+        value: T,
+        branches: CaseBranch<T>[],
+        defaultBranch: ComponentFactory | null
+        ) {
+            this._value = value
+            this.branches = branches
+            this.defaultBranch = defaultBranch
+        this.placeholder = plh(this._selectBranch())(renderer)
+    }
+    
+    get value() {
+        return this._value
+    }
+    
+    
+    set value(value: T) {
+        if (this._value !== value) {
+            this.placeholder.setContent(this._selectBranch())
+            this._value = value
+        }
+    }
+
+    _selectBranch() {
+        for (const branch of this.branches) {
+            if (this.value === branch[0]) {
+                return branch[1]
+            }
+        }
+        return this.defaultBranch
+    }
+}
+
+
+export const switchElse =
+    <T>(
+        value: T,
+        branches: CaseBranch<T>[],
+        defaultBranch: ComponentFactory | null = null
+    ): ComponentFactory<Switch<T>> =>
+    (renderer: Renderer) =>
+        new SwitchImpl(renderer, value, branches, defaultBranch)
