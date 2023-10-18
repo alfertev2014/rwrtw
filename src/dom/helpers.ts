ca98ed6949950d34a5b8851517806b6fd34a78c8
Василий <valfertev@rtk-element.ru>
2023-10-18 18:11:51 +0300

    WIP Helpers to create dom trees without lifecycles.

--- src/dom/helpers.ts
+++ src/dom/helpers.ts
@@ -1,3 +1,5 @@
+import { ParentNodePlace, Place, renderNode } from "../internal/place"
+
 /**
  * Value type for elements' attributes.
  */
@@ -33,3 +35,49 @@ export const dce = (tag: string) => document.createElement(tag)
  * @returns HTML text node.
  */
 export const txt = (str: string) => document.createTextNode(str)
+
+export interface ElementAttrsMap {
+    [key: string]: ElementAttrValue
+}
+
+export type ChildrenItem = HTMLElement | ElementAttrValue
+export type ChildrenType = ChildrenItem | ChildrenType[]
+
+export const hel =
+    (tag: string, attrs: ElementAttrsMap | null = null, ...handlers: ((e: HTMLElement) => void)[]) =>
+    (...children: ChildrenType[]): Place => {
+    const element = dce(tag)
+
+    processRendered(new ParentNodePlace(element), children)
+
+    if (attrs) {
+        for (const [name, value] of Object.entries(attrs)) {
+            setAttr(element, name, value)
+        }
+    }
+    for (const handler of handlers) {
+        handler(element)
+    }
+    return element
+}
+
+const processRendered = (
+    place: Place,
+    rendered: ChildrenType,
+): Place => {
+    if (typeof rendered === 'boolean' || rendered === null || typeof rendered === 'undefined') {
+        return place
+    }
+    if (typeof rendered === 'string') {
+        place = renderNode(place, txt(rendered))
+    } else if (typeof rendered === 'number') {
+        place = renderNode(place, txt(rendered.toString()))
+    } else if (Array.isArray(rendered)) {
+        for (const r of rendered) {
+            place = processRendered(place, r)
+        }
+    } else {
+        place = renderNode(place, rendered)
+    }
+    return place
+}
