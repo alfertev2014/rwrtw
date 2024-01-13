48c1fb8142979da170098a288e5c125738351c0c
Василий <alfertev2012@gmail.com>
2024-01-13 16:11:24 +0300

    Refactor template lang. Add element item type again.

--- src/template/index.ts
+++ src/template/index.ts
@@ -8,34 +8,73 @@ import {
   appendNodeAt,
   type Place,
   placeAtBeginingOf,
+  type PlaceholderComponent,
 } from "../core/index.js"
 
-export type RenderedType = "placeholder" | "list" | "component"
+export type TemplateItemType = "element" | "placeholder" | "list" | "component"
 
-export interface RenderedPlaceholder {
+export interface ElementTemplateItem {
+  type: "element"
+  tag: string
+  attrs: TemplateElementAttrsMap | null
+  children: TemplateItem[]
+  handler?: (element: HTMLElement) => void
+}
+
+export const el =
+  (tag: string, attrs: TemplateElementAttrsMap | null = null, handler: ElementHandler) =>
+  (...children: TemplateItem[]): ElementTemplateItem => ({
+    type: "element",
+    tag,
+    attrs,
+    handler,
+    children,
+  })
+
+export interface PlaceholderTemplateItem {
   type: "placeholder"
   content: PlaceholderContent
   handler?: (placeholder: Placeholder) => void
-  context: PlaceholderContext
 }
 
-export interface RenderedList {
+export const plh = (
+  content: PlaceholderContent,
+  handler?: (placeholder: Placeholder) => void,
+): PlaceholderTemplateItem => ({
+  type: "placeholder",
+  content,
+  handler,
+})
+
+export interface ListTemplateItem {
   type: "list"
   contents: PlaceholderContent[]
   handler?: (list: PlaceholderList) => void
-  context: PlaceholderContext
 }
 
-export interface RenderedComponent {
+export const list = (
+  contents: PlaceholderContent[],
+  handler?: (list: PlaceholderList) => void,
+): ListTemplateItem => ({
+  type: "list",
+  contents,
+  handler,
+})
+
+export interface ComponentTemplateItem {
   type: "component"
   content: PlaceholderContent
-  context: PlaceholderContext
 }
 
-export type TemplateItem = ScalarValue | Node | RenderedPlaceholder | RenderedList | RenderedComponent
+export const cmpnt = (content: PlaceholderContent): ComponentTemplateItem => ({
+  type: "component",
+  content,
+})
+
+export type TemplateItem = ScalarValue | PlaceholderTemplateItem | ListTemplateItem | ComponentTemplateItem | ElementTemplateItem
 
 export type ElementHandler = (element: HTMLElement) => void
-export type AttributeHandler = (element: HTMLElement, attr: string) => void
+export type AttributeHandler = (attr: string, element: HTMLElement, context: PlaceholderContext) => void
 
 export type TemplateElementAttrsMap = Record<string, ScalarValue | AttributeHandler>
 
@@ -47,7 +86,7 @@ export const on =
     return res
   }
 
-const processRendered = (place: Place, content: TemplateItem[]): Place => {
+const renderTemplateItems = (place: Place, context: PlaceholderContext, content: TemplateItem[]): Place => {
   for (const rendered of content) {
     if (typeof rendered === "boolean" || rendered == null) {
       return place
@@ -56,75 +95,51 @@ const processRendered = (place: Place, content: TemplateItem[]): Place => {
       place = appendNodeAt(place, txt(rendered))
     } else if (typeof rendered === "number") {
       place = appendNodeAt(place, txt(rendered.toString()))
-    } else if (rendered instanceof Node) {
-      place = appendNodeAt(place, rendered)
+    } else if (rendered.type === "element") {
+      place = renderElementItem(rendered, place, context)
     } else if (rendered.type === "placeholder") {
-      const plh = rendered.context.createPlaceholderAt(place, rendered.content)
+      const plh = context.createPlaceholderAt(place, rendered.content)
       rendered.handler?.(plh)
       place = plh
     } else if (rendered.type === "list") {
-      const list = rendered.context.createListAt(place, rendered.contents)
+      const list = context.createListAt(place, rendered.contents)
       rendered.handler?.(list)
       place = list
     } else if (rendered.type === "component" && rendered.content != null) {
-      place = rendered.content(place, rendered.context)
+      place = rendered.content(place, context)
     }
   }
   return place
 }
 
-export const el =
-  (tag: string, attrs: TemplateElementAttrsMap | null = null, ...handlers: ElementHandler[]) =>
-  (...children: TemplateItem[]): HTMLElement => {
-    const element = dce(tag)
+const renderElementItem = (
+  { tag, attrs, handler, children }: ElementTemplateItem,
+  place: Place,
+  context: PlaceholderContext,
+): HTMLElement => {
+  const element = dce(tag)
 
-    processRendered(placeAtBeginingOf(element), children)
+  renderTemplateItems(placeAtBeginingOf(element), context, children)
 
-    if (attrs != null) {
-      for (const [name, value] of Object.entries(attrs)) {
-        if (typeof value === "function") {
-          value(element, name)
-        } else {
-          setAttr(element, name, value)
-        }
+  if (attrs != null) {
+    for (const [name, value] of Object.entries(attrs)) {
+      if (typeof value === "function") {
+        value(name, element, context)
+      } else {
+        setAttr(element, name, value)
       }
     }
-
-    for (const handler of handlers) {
-      handler(element)
-    }
-    return element
   }
 
-export const plh = (
-  context: PlaceholderContext,
-  content: PlaceholderContent,
-  handler?: (placeholder: Placeholder) => void,
-): RenderedPlaceholder => ({
-  type: "placeholder",
-  content,
-  handler,
-  context,
-})
+  appendNodeAt(place, element)
 
-export const list = (
-  context: PlaceholderContext,
-  contents: PlaceholderContent[],
-  handler?: (list: PlaceholderList) => void,
-): RenderedList => ({
-  type: "list",
-  contents,
-  handler,
-  context,
-})
+  handler?.(element)
+  return element
+}
 
-export const cmpnt = (context: PlaceholderContext, content: PlaceholderContent): RenderedComponent => ({
-  type: "component",
-  content,
-  context,
-})
-
-export const fr = (place: Place, ...content: TemplateItem[]): Place => processRendered(place, content)
+export const renderTemplate = (...content: TemplateItem[]): PlaceholderComponent => (place, context) => {
+  return renderTemplateItems(place, context, content)
+}
 
 export interface TemplateRef<T> {
   current: T
