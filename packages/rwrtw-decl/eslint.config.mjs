4c6d9df57e45c6625d9be40561ca3713a168aee4
Василий <alfertev2012@gmail.com>
2026-08-24 00:33:47 +0300

    Upgrade dependencies. Fix compilation.

--- packages/rwrtw-decl/eslint.config.mjs
+++ packages/rwrtw-decl/eslint.config.mjs
@@ -1,8 +1,5 @@
 import baseConfig from "../../eslint.config.mjs"
 
 export default [
-  ...baseConfig,
-  {
-    ignores: ["**/out-tsc", "src/ui-compiler/**"],
-  },
+  ...baseConfig
 ]
--- packages/rwrtw-decl/src/index.ts
+++ packages/rwrtw-decl/src/index.ts
@@ -1,3 +1,1 @@
 export * from "./ast/index.js"
-export * from "./ui-model/template.js"
-export * from "./ui-model/transformer.js"
